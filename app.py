2eacc923cb57c9354296f573e9dd61a236e1e5bc
Essi Savonius <essi.savonius@hotmail.fi>
2023-09-14 17:38:37 +0300

    file organizing and some css added

--- app.py
+++ app.py
@@ -1,69 +1,8 @@
 from os import getenv
-from sqlalchemy.sql import text
 from flask import Flask
-from flask import redirect, render_template, request, session
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-db = SQLAlchemy(app)
 app.secret_key = getenv("SECRET_KEY")
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/open_login")
-def open_login():
-    return render_template("login.html")
-
-@app.route("/open_register")
-def open_register():
-    return render_template("register.html")
-
-@app.route("/login",methods=["POST"])
-def login():
-    username = request.form["username"]
-    password = request.form["password"]
-
-    # If username and password incorrect, new login page
-
-    sql = text("SELECT id, password FROM users WHERE username=:username")
-    result = db.session.execute(sql, {"username":username})
-    user = result.fetchone()    
-    if not user:
-        pass
-    else:
-        hash_value = user.password
-        if check_password_hash(hash_value, password):
-            session["username"] = username
-            return render_template("new.html")
-    
-    return render_template("login.html")
-
-@app.route("/logout")
-def logout():
-    del session["username"]
-    return redirect("/")
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    username = request.form["username"]
-    password = request.form["password"]
-    sql = text("SELECT id, password FROM users WHERE username=:username")
-    result = db.session.execute(sql, {"username":username})
-    user = result.fetchone()    
-    if not user:
-        createUser(username, password)
-        session["username"] = username
-        return render_template("new.html")
-    
-    return render_template("register.html")
-
-def createUser(username, password):
-    hash_value = generate_password_hash(password)
-    sql = text("INSERT INTO users (username, password) VALUES (:username, :password)")
-    db.session.execute(sql, {"username":username, "password":hash_value})
-    db.session.commit()
+import routes
 
---
+++ routes.py
@@ -0,0 +1,38 @@
+from app import app
+from flask import render_template, request, redirect, session
+import users
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/open_login")
+def open_login():
+    return render_template("login.html")
+
+@app.route("/open_register")
+def open_register():
+    return render_template("register.html")
+
+@app.route("/login",methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    if users.login(username, password):
+        session["username"] = username
+        return render_template("new.html")
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    username = request.form["username"]
+    password = request.form["password"]
+    if users.register(username, password):
+        session["username"] = username
+        return render_template("new.html")
+    return render_template("register.html")
