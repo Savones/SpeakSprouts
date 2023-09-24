4169dc7cf8dcbdba98eeae6e220ae6bd7e51a945
Essi Savonius <essi.savonius@hotmail.fi>
2023-09-24 14:35:07 +0300

    styling changes

--- testing.py
+++ testing.py
@@ -1,20 +1,14 @@
-import random
-import string
 from db import db
 from sqlalchemy.sql import text
 import users
 
-def generate_random_string(length=10):
-    letters = string.ascii_letters
-    return ''.join(random.choice(letters) for _ in range(length))
-
-def create_dummy_users(num_users=10):
+def create_dummy_users():
     sql = text("SELECT * FROM Users")
     result = db.session.execute(sql).fetchall()
 
     if len(result) < 10:
-        for _ in range(num_users):
-            username = generate_random_string()
+        usernames = ["Maria", "Mikko", "Petteri", "Jaana", "Alisa", "Katariina", "Jaakko", "Miia", "Matti-Pekka", "Anniina"]
+        for username in usernames:
             users.register(username, username)
         
         db.session.commit()
